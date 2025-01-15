988908840e0823ade2371ad374c6d9bc90c6b557
Dilbag Singh <dilbag.boperai@gmail.com>
2025-01-15 16:05:24 +0530

    add mirgations

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
-    "debug_toolbar"
+    'debug_toolbar',
+    'likes',
+    'store',
+    'tags'
 ]
 
 MIDDLEWARE = [
@@ -48,8 +51,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+   
 ]
 
 ROOT_URLCONF = 'storefront.urls'
@@ -83,11 +86,14 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'Matrid248142',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
